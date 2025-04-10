cffa6fa3c0fd1cd189e0a41d56aa042efac4c462
Élcio Reis <elcio.reis024@gmail.com>
2025-04-10 09:03:34 -0300

    add metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,7 +5,7 @@ import LayoutClient from "./layoutClient";
 
 export const metadata: Metadata = {
   title: "Energy on",
-  description: "A melhor empresa de energia de Caxias.",
+  description: "A melhor empresa de energia de Caxias-MA.",
   openGraph: {
     title: "Energy On - Criada por ElcioServiçosOn",
     description: "Conheça nosso sistema.",
@@ -13,7 +13,7 @@ export const metadata: Metadata = {
     type: "website",
     images: [
       {
-        url: "https://energy-on-elcioservicos.netlify.app/_next/image?url=%2Fassets%2Fimages%2Flogomarca.png&w=96&q=75",
+        url: "https://energy-on-elcioservicos.netlify.app/_next/image?url=%2Fassets%2Fimages%2Flogomarca.png&w=256&q=75",
         width: 256,
         height: 256,
         alt: "Logomarca",
