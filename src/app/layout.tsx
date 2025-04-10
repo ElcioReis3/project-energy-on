dff7752b1fe9e376dee845816b71b6f45d040f40
Élcio Reis <elcio.reis024@gmail.com>
2025-04-10 08:51:07 -0300

    add tags

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,18 +4,16 @@ import "./globals.css";
 import LayoutClient from "./layoutClient";
 
 export const metadata: Metadata = {
-  title: "Empresa X",
-  description:
-    "Com anos de experiência no mercado automotivo, nossa missão é conectar você ao carro dos seus sonhos com transparência, segurança e condições imperdíveis.",
+  title: "Energy on",
+  description: "A melhor empresa de energia de Caxias.",
   openGraph: {
-    title: "Nome da sua Empresa",
-    description:
-      "Com anos de experiência no mercado automotivo, nossa missão é conectar você ao carro dos seus sonhos com transparência, segurança e condições imperdíveis.",
-    url: "https://marketplace-cars.netlify.app/",
+    title: "Energy On - Criada por ElcioServiçosOn",
+    description: "Conheça nosso sistema.",
+    url: "https://energy-on-elcioservicos.netlify.app/",
     type: "website",
     images: [
       {
-        url: "https://marketplace-cars.netlify.app/_next/image?url=%2Fassets%2Fimages%2Fcar.png&w=640&q=75",
+        url: "https://energy-on-elcioservicos.netlify.app/_next/image?url=%2Fassets%2Fimages%2Flogomarca.png&w=96&q=75",
         width: 256,
         height: 256,
         alt: "Logomarca",
